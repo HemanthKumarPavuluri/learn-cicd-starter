de5f98f334ce56b09a516077f47cca288cc23d0f
HemanthKumarPavuluri <phk.phk007@gmail.com>
2025-09-08 20:38:26 -0500

    modified migrations script to be  compatible with goose

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -4,5 +4,10 @@ if [ -f .env ]; then
     source .env
 fi
 
-cd sql/schema
-goose turso $DATABASE_URL up
+# Authenticate with Turso CLI
+turso auth token "$TURSO_TOKEN"
+
+# Apply migrations using Turso CLI
+for file in sql/schema/*.sql; do
+  turso db shell "$TURSO_DB_NAME" < "$file"
+done
